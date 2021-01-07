a8cea7fe539f5527a6c4ab65b7affeabce58991a
Traian <niculescu.traian.ct@gmail.com>
2021-01-07 12:09:10 +0200

    useEffect dependencies

--- dojo-blog/src/components/Home/Home.js
+++ dojo-blog/src/components/Home/Home.js
@@ -7,15 +7,23 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Webdev tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
+  const [name, setName] = useState('mario');
   useEffect(() => {
-    console.log('useEffect ran');
-  });
+    console.log('useEffect ran: ', name);
+  }, [name]);
   const handleDelete = (id) => {
     setBlogs((prevBlogs) => prevBlogs.filter((blog) => blog.id !== id));
   };
   return (
     <div className='home'>
       <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
+      <button
+        onClick={() =>
+          setName((prevName) => (prevName === 'mario' ? 'luigi' : 'mario'))
+        }
+      >
+        change name
+      </button>
     </div>
   );
 };
