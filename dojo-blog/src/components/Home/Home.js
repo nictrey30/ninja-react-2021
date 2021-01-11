9709a1ba7ea1fad728ee95213166982cc1cf9817
Traian <niculescu.traian.ct@gmail.com>
2021-01-11 19:49:37 +0200

    conditional loading message

--- dojo-blog/src/components/Home/Home.js
+++ dojo-blog/src/components/Home/Home.js
@@ -4,26 +4,32 @@ import axios from 'axios';
 
 const Home = () => {
   const [blogs, setBlogs] = useState('');
+  const [isPending, setIsPending] = useState(true);
 
   // we can't use async in useEffect hook
   useEffect(() => {
-    fetchData()
-      .then((result) => setBlogs(result))
-      .catch((err) => console.log(err));
+    setTimeout(() => {
+      fetchData()
+        .then((result) => {
+          setBlogs(result);
+          // once we have the data back set the isPending to false to hide the loading message
+          setIsPending(false);
+        })
+        .catch((err) => console.log(err));
+    }, 1000);
   }, []);
 
   const handleDelete = (id) => {
-    // setBlogs((prevBlogs) => prevBlogs.filter((blog) => blog.id !== id));
     axios
       .delete(`http://localhost:8000/blogs/${id}`)
-      .then((data) => {
+      .then(() => {
         setBlogs((prevBlogs) => prevBlogs.filter((blog) => blog.id !== id));
       })
       .catch((err) => console.log(err));
   };
-
   return (
     <div className='home'>
+      {isPending && <div>Loading...</div>}
       {blogs && (
         <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
       )}
